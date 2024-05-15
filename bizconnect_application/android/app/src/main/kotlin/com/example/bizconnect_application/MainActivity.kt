7169ebd4b023c56950df1bee5418a3c4d1cc4809
Sahri Ramadan <sahriramadan000@gmail.com>
2024-05-15 15:06:15 +0700

    (fix): Fix bug structure dart

--- bizconnect_application/android/app/src/main/kotlin/com/example/bizconnect_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bizconnect_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- bizconnect_application/lib/models/user_model.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  String setup;
-  String punchline;
-  
-  User({required this.setup, required this.punchline});
-}
